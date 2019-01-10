dc8ae1bff3a2db6ec45b3133604f73852bb6a65a
Kadam Parikh <39589393+ParikhKadam@users.noreply.github.com>
2019-01-10 18:57:21 +0530

    Delete data_generator.py

--- scripts/data_generator.py
+++
@@ -1,7 +0,0 @@
-from ..scripts import BatchGenerator
-
-
-def load_data_generators(batch_size, emdim):
-    train_generator = BatchGenerator('train', batch_size, emdim)
-    validation_generator = BatchGenerator('dev', batch_size, emdim)
-    return train_generator, validation_generator
