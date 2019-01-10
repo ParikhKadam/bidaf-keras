12f7a2abbcbee389330dca448fb0c9fbd5830c13
Kadam Parikh <39589393+ParikhKadam@users.noreply.github.com>
2019-01-10 18:57:20 +0530

    Delete __init__.py

--- scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from .batch_generator import BatchGenerator
-from .loss_function import negative_avg_log_error
-from .magnitude import MagnitudeVectors
-from .data_generator import load_data_generators
