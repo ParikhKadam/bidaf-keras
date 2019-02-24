f71afded57924c7f0d88666d991c70d1ff3f6d6e
Kadam Parikh <39589393+ParikhKadam@users.noreply.github.com>
2019-02-24 14:36:59 +0530

    [UPDATE] Added multi gpu wrapper class for easy saving/loading of model

--- bidaf/scripts/__init__.py
+++ bidaf/scripts/__init__.py
@@ -3,3 +3,4 @@ from .loss_function import negative_avg_log_error
 from .magnitude import MagnitudeVectors
 from .data_generator import load_data_generators
 from .accuracy_metric import accuracy
+from .multi_gpu_model import ModelMGPU
---
+++ bidaf/scripts/multi_gpu_model.py
@@ -0,0 +1,19 @@
+from keras import Model
+from keras.utils import multi_gpu_model
+
+
+class ModelMGPU(Model):
+    def __init__(self, ser_model, gpus=None):
+        pmodel = multi_gpu_model(ser_model, gpus)
+        self.__dict__.update(pmodel.__dict__)
+        self._smodel = ser_model
+
+    def __getattribute__(self, attrname):
+        '''Override load and save methods to be used from the serial-model. The
+        serial-model holds references to the weights in the multi-gpu model.
+        '''
+        # return Model.__getattribute__(self, attrname)
+        if 'load' in attrname or 'save' in attrname:
+            return getattr(self._smodel, attrname)
+
+        return super(ModelMGPU, self).__getattribute__(attrname)
